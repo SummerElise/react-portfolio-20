3d25d020f6444de596768c244a6f182ab9d56e33
lissy <slissy513@gmail.com>
2021-08-16 16:31:58 -0400

    added necessary files and folders. Coded and completed in PortfolioContainer, NavTabs, and Header.

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+// import '../styles/Header.css';
+
+const styles = {
+  headerStyle: {
+    background: 'light-blue',
+  },
+  headingStyle: {
+    fontSize: '100px',
+  },
+  headingStyle2: {
+    fontSize: '45px',
+  },
+};
+
+// add images and proper css
+
+function Header() {
+  return (
+    <header style={styles.headerStyle} className="header">
+      <h1 style={styles.headingStyle}>Welcome to my React Portfolio! I'm Summer.</h1>
+      <h2 style={styles.headingStyle2}>I am a full-stack web developer.</h2>
+    </header>
+  );
+}
+
+export default Header;
